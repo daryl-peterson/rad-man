f257899fba322c44b4e566925c1fd3ddfb29b50d
daryl-peterson <daryl.peterson@gmail.com>
2023-10-01 07:53:11 -0500

    Update

--- app/Core/Traits/Operator.php
+++ app/Core/Traits/Operator.php
@@ -4,6 +4,15 @@ namespace App\Core\Traits;
 
 use App\Core\Exceptions\InvalidOperator;
 
+/**
+ * Trait to validate op variable.
+ *
+ * @author      Daryl Peterson <@gmail.com>
+ * @copyright   Copyright (c) 2020, Daryl Peterson
+ * @license     https://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * @since       1.0.0
+ */
 trait Operator
 {
     public static $VALID_OPS = [
